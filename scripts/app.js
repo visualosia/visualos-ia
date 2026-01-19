aa17ac19b8afea53cbfcff1c5fd8b14dfd499522
visualosia <michalocampo93@gmail.com>
2026-01-19 10:56:20 -0600

    datos simulados de pacientes

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,8 @@
 function saludar() {
   alert("VisualOS IA activo");
 }
+let pacientes = [
+  { nombre: "Juan Pérez", edad: 45 },
+  { nombre: "María López", edad: 32 },
+  { nombre: "Carlos Hernández", edad: 60 }
+];
